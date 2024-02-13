7b1ef60fb602561550a5831cf64c52b9522bc107
nicholasmhughes <nicholasmhughes@gmail.com>
2024-02-12 19:25:11 -0500

    add utils unit tests

--- tests/unit/utils/github.py
+++
@@ -1,6 +0,0 @@
-from saltext.github.utils import github
-
-def test_get_user_pubkeys():
-    # should fail, becausse we only want to allow a list
-    users = {}
-    assert(github.get_user_pubkeys(users) == {"Error": "A list of users is expected"})
